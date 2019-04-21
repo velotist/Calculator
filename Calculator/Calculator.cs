b1021aa38d9d11e0050eb58e2c2eaa61ed97673d
Velotist <velotist@aikq.de>
2019-04-21 20:45:32 +0200

    Projektdateien hinzufügen.

---
+++ Calculator/Calculator.cs
@@ -0,0 +1,122 @@
+﻿using System;
+
+namespace Calculator
+{
+    class Calculator
+    {
+        public void Run()
+        {
+            // Beginn einer Endlosschleife
+            while(true)
+            {
+                double number1, number2, result;
+                char operation;
+                // Startbedingungen für die Berechnung
+                // wenn die Methoden NumberInput() und OperationInput() ein true zurücklieferten, dann wird
+                // der Anweisungsblock in switch/case abgearbeitet
+                // die zweimalige logische Und-Verknüpfung mit der Methode NumberInput deshalb, weil wir sie für
+                // die Eingabe der ersten und der zweiten Zahl aufrufen
+                if (NumberInput(out number1) && OperationInput(out operation) && NumberInput(out number2))
+                {
+                    // we do calculation
+                    switch(operation)
+                    {
+                        case '+':
+                            result = number1 + number2;
+                            break;
+                        case '-':
+                            result = number1 - number2;
+                            break;
+                        case '*':
+                            result = number1 * number2;
+                            break;
+                        case '/':
+                            result = number1 / number2;
+                            break;
+                        case '%':
+                            result = number1 % number2;
+                            break;
+                        default:
+                            // wir benötigen hier eine Zuweisung für die result-Variable, da der Compiler sonst
+                            // eine Fehlermeldung ausgibt, dass er eine Zuweisung braucht
+                            result = 0.0;
+                            break;
+                    }
+                    Console.WriteLine("Ergebnis       {0}",result);
+                    Console.WriteLine();
+                }
+                else
+                {
+                    Console.WriteLine("Bye bye");
+                    break;
+                }
+            }
+        }
+
+        // die Methode für die Überprüfung einer korrekten Eingaben; bei Korrektheit
+        // der Eingabe wird true zurückgeliefert
+        private bool NumberInput(out double number)
+        {
+            // Beginn einer Endlosschleife; diese wird durch die Eingabe des Zeichens q beendet
+            while(true)
+            {
+                // Abfrage der Nutzereingabe
+                Console.Write("Zahl           ");
+                string input = Console.ReadLine();
+                // wenn ein q eingegeben wurde, dann liefert die Methode false zurück
+                // q steht hier für quit und beendet das Programm, da in der Methode Run()
+                // die logische Und-Verknüpfung nicht mehr erfüllt ist und das Programm
+                // aus der Endlosschleife in Run() aussteigt
+                if (input.Equals("q"))
+                {
+                    number = 0.0;
+                    return false;
+                }
+                if (double.TryParse(input, out number))
+                {
+                    break;
+                }
+                // bei einer Fehleingabe wird eine lesbare Fehlerrückmeldung an den Nutzer erzeugt
+                else
+                {
+                    Console.WriteLine("Die Eingabe war fehlerhaft. Versuchen Sie es nochmal. Hier bitte nur Zahlen eingeben.");
+                }
+            }
+            // wenn alles passt, gib einen true Wert zurück
+            return true;
+        }
+
+        // die Methode für die Prüfung der korrekten Eingabe der Operatoren
+        private bool OperationInput(out char operation)
+        {
+            string operationen = "+-*/%";
+            // Beginn einer Endlosschleife
+            while(true)
+            {
+                Console.Write("Operator        ");
+                string input = Console.ReadLine();
+                if (input.Equals("q"))
+                {
+                    operation = '0';
+                    return false;
+                }
+                // wenn die Länge der Eingabe = eins ist (damit wird die Eingabe von zwei oder mehr Operatoren
+                // abgefangen) und der Operator ein Zeichen des Strings enthält
+                // dann weise der Variablen den passenden Operator zu
+                if (input.Length == 1 && operationen.Contains(input))
+                {
+                    operation = input[0];
+                    break;
+                }
+                // ansonsten gib eine lesbare Fehlermeldung an den Nutzer aus
+                else
+                {
+                    Console.WriteLine("Die Eingabe war fehlerhaft. Versuchen Sie es nochmal. Wählen Sie bitte zwischen +, -, *, / und %.");
+                }
+            }
+            // wenn alles passt gib true zurück für die Abarbeitung der Run()-Methode
+            return true;
+        }
+    }
+}
+
---
+++ Calculator/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+/*  Author:     Konstantin Golovnenkov && Marcus Greiner
+ *  
+ *  Version:    1.0
+ *  Date:      08.03.2019
+ *  
+ *  A little calculator in the console for operations in additon, subtraction,
+ *  multiplication, division and modulo of two double or int numbers.
+ *  
+ */
+
+namespace Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Calculator calc1 = new Calculator();
+            Console.WriteLine("Konsolen-Taschenrechner");
+            Console.WriteLine("Sie werden nach der ersten Zahl gefragt.");
+            Console.WriteLine("Anschließend können Sie einen der folgenden Operatoren wählen:");
+            Console.WriteLine();
+            Console.WriteLine("                 +    -   *   /   %");
+            Console.WriteLine();
+            Console.WriteLine("Wollen Sie beginnen? Jederzeit q fürs Beenden.");
+            Console.Write("Press key... ");
+            Console.ReadKey();
+            Console.WriteLine();
+            Console.WriteLine("Okay. Bitte erste Zahl eingeben, mit Enter bestätigen, danach den Operator und dann die zweite Zahl.");
+            Console.WriteLine("Fürs Beenden könne Sie jederzeit die Taste q betätigen.");
+            Console.WriteLine();
+            calc1.Run();
+        }
+    }
+}
